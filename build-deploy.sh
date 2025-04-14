8549da09ce94e644902ec9815ceec085a3b8b6e9
p-nasi <pnasimonan@gmail.com>
2025-04-14 09:59:26 +0900

    [update]deploy.sh

--- build-deploy.sh
+++ build-deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#yokan-pageの中で実行
 
 # Viteプロジェクトをビルド
 npm run build
@@ -9,5 +10,4 @@ cd dist
 # パーミッションを変更
 chmod -R 755 *
 
-# 元のディレクトリに戻る
-cd ..
+scp -r * amane:/home/student/e24/e245719/public_html
